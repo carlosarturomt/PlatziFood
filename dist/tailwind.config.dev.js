f219829dc97371461ac78edabb56d725365deb44
CarlosArturoMT <carlosarturomt@gmail.com>
2021-08-03 17:22:50 -0500

    tarjeta y header

---
+++ dist/tailwind.config.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  purge: [],
+  darkMode: false,
+  // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
+    },
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secundary': '#353535'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors:{
+        'primary':'#f3c614',
+        'secundary':'#353535'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
